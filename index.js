3eb378aaf4cce1691af0971506a63be9ee27112d
Johansel <johanselp24@gmail.com>
2024-04-08 09:48:53 -0400

    .

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 
 
-var app = require('./app');
+let app = require('./app');
 
-var port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 //ESTO ES PARA CONECTARNOS A MONGOOSE POR MEDIO DE LAS PROMESAS
 // mongoose.Promise = global.Promise;
 // mongoose.set('useFindAndModify',false);
 
-app.listen(port,'0.0.0.0',()=>{
+let connection = require('./controllers/connection') 
+
+let socket  = require('socket.io')
+
+let server = app.listen(port,'0.0.0.0',()=>{
     console.log(port)
     console.log('El puerto de 3700, ya esta listo..');
+})
+
+let socketIo = socket(server);
+
+socketIo.on('connection',()=>{
+    console.log('La conexion ha sido creada con el socket: ' + socket.id)
+
+
+    // socket.on(connection.change,(change) => {
+    //     socketIo.socket.emit(connection.change, change)
+    // })
+    socket.emit("hello", "world", (response) => {
+        console.log(response); // "got it"
+      });
+
+    socket.on(connection.create,(newData)=>{
+        socketIo.socket.emit(connection.create, newData)
+    })
+
+
+
 })
 
 
