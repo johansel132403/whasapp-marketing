391dc7bec7b11718b66c5a072c797e2682f32761
Johansel <johanselp24@gmail.com>
2024-04-24 13:59:43 -0400

    .

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -171,7 +171,7 @@ let controllers  = {
         // req.body {"object":"whatsapp_business_account","entry":[{"id":"249865991547503","changes":[{"value":{"messaging_product":"whatsapp","metadata":{"display_phone_number":"18496420776","phone_number_id":"208174665722024"},"contacts":[{"profile":{"name":"Hanck"},"wa_id":"18093199970"}],"messages":[{"from":"18093199970","id":"wamid.HBgLMTgwOTMxOTk5NzAVAgASGBQzQUQzNkQ0MTQwNEVFNDY1N0JCRQA=","timestamp":"1713813794","text":{"body":"B"},"type":"text"}]},"field":"messages"}]}]}
 
 
-        if(req){
+        if(req.body){
           
                   let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
                   let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
