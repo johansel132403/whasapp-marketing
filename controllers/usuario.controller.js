7d8de1b72cab3259c97b7ed791e4cfc501157139
Johansel <johanselp24@gmail.com>
2024-04-26 16:41:58 -0400

    .

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -166,7 +166,7 @@ let controllers  = {
       
       receivPosteMessage: async function( req, res){
         let body =     JSON.stringify(req.body)
-        console.log("req.Body",body.object)
+        console.log("req.Body",body)
         console.log("entry",body.entry)
         console.log("req.body.entry[0].changes",req.body.entry[0].changes)
 
