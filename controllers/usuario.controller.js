43d085181aae2334bee5bb6fbd96b2a8c0659691
Johansel <johanselp24@gmail.com>
2024-04-27 13:59:16 -0400

    .

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -205,7 +205,7 @@ let controllers  = {
           console.log(obj.entry[0].changes[0].value.statuses[0].status)
           console.log(obj.entry[0].changes[0].value.statuses[0].timestamp)
 
-            if( req.body.entry[0].changes[0].value.messages[0].from && req.body.entry[0].changes[0].value.messages[0].text.body){
+            if( req.body.entry[0].changes[0].value.messages[0].from !== 'undefined ' && req.body.entry[0].changes[0].value.messages[0].text.body !== 'undefined' ){
 
               let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
               let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
