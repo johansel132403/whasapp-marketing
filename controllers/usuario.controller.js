1a989bd2537ac1945f1a46a5f3bef99b983a295a
Johansel <johanselp24@gmail.com>
2024-05-16 13:34:32 -0400

    .

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -249,7 +249,7 @@ let controllers  = {
                  chat.IdChat = from;
                   chat.Emisor = datos
 
-                  let chats = false;
+                 
 
                       // let chats = await Chat.find({"IdChat":req.body.entry[0].changes[0].value.messages[0].from}).exec().then((response)=>{
                       //               return response;
@@ -258,37 +258,38 @@ let controllers  = {
                       let update = await Chat.findOneAndUpdate({"IdChat":req.body.entry[0].changes[0].value.messages[0].from},{$push:{"Emisor":datos}}).exec().then((response)=>{
                         return response;
                         });
-                        console.log('confirmcc:', update)
+                    //     console.log('confirmcc:', update)
 
-                        if(update){
-                          console.log('confirm:', update)
-                          return res.status(200).send({Mensaje:"Updated !"});
+                    //     if(update){
+                    //       console.log('confirm:', update)
+                    //       return res.status(200).send({Mensaje:"Updated !"});
 
-                     }
+                    //  }
                    
 
 
-                      if(chats){
+                      if(update !== null){
 
-                        let datos = [{
-                          Nombre: req.body.entry[0].changes[0].value.contacts[0].profile.name,
-                          messagesID: req.body.entry[0].changes[0].value.messages[0].id,
-                         numero:  req.body.entry[0].changes[0].value.messages[0].from,
-                         msgText:  req.body.entry[0].changes[0].value.messages[0].text.body,
-                         timestamp: req.body.entry[0].changes[0].value.messages[0].timestamp,
-                        }]
+                      //   let datos = [{
+                      //     Nombre: req.body.entry[0].changes[0].value.contacts[0].profile.name,
+                      //     messagesID: req.body.entry[0].changes[0].value.messages[0].id,
+                      //    numero:  req.body.entry[0].changes[0].value.messages[0].from,
+                      //    msgText:  req.body.entry[0].changes[0].value.messages[0].text.body,
+                      //    timestamp: req.body.entry[0].changes[0].value.messages[0].timestamp,
+                      //   }]
 
-                       chat.IdChat = from;
-                        chat.Emisor = datos
+                      //  chat.IdChat = from;
+                      //   chat.Emisor = datos
                      
-                       let update = await Chat.findOneAndUpdate({"IdChat":req.body.entry[0].changes[0].value.messages[0].from},{$push:{"Emisor":datos}}).exec().then((response)=>{
-                                    return response;
-                         });
+                      //  let update = await Chat.findOneAndUpdate({"IdChat":req.body.entry[0].changes[0].value.messages[0].from},{$push:{"Emisor":datos}}).exec().then((response)=>{
+                      //               return response;
+                      //    });
 
-                         if(update){
+                        //  if(update){
+                        //       return res.status(200).send({Mensaje:"Updated !"});
+
+                        //  }
                               return res.status(200).send({Mensaje:"Updated !"});
-
-                         }
                        
 
                       }else{
@@ -305,8 +306,8 @@ let controllers  = {
                                   // chat.Emisor = datos
                               
                                 
-                                  // let  output = await chat.save();
-                                  // return res.status(200).send({Mensaje:"Saved!"});
+                                  let  output = await chat.save();
+                                  return res.status(200).send({Mensaje:"Saved!"});
                             }
                       }
                   catch(e){
