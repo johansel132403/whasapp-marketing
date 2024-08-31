59f578fc280a7b4103c985067fc298a22304b8ea
Johansel <johanselp24@gmail.com>
2024-08-31 14:28:03 -0400

    update model

--- models/chatModel.js
+++ models/chatModel.js
@@ -18,6 +18,18 @@ let userSchema = Schema({
         timestamp: String,
         imagen: String,
     }],
+    type:String,
+    data:[{
+        nombre: String,
+        header: String,             
+        imagen: String,
+        body: String,
+        timestamp: String,
+        foote:String,
+        bottom:String,
+    }],
+
+
    
 
     
