141653a04556fd34410c181ce1d196456b6825c1
Johansel <johanselp24@gmail.com>
2024-09-02 12:50:30 -0400

    *

--- models/chatModel.js
+++ models/chatModel.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 // Conectarse a la base de datos
-mongoose.connect('mongodb://localhost:27017/odontoarte24:odontoarte24@cluster0.tvfjxum.mongodb.net/whatsappmarketing', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
